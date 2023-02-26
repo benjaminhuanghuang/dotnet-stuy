0d4a82085efc936efa90ebffc763880f7f587ac5
BenjaminHuang <benjaminhuanghuang@gmail.com>
2023-02-26 14:42:34 -0800

    update

---
+++ Palyground/ConsoleInputOutput/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+Console.Write("Please enter your age");
+int age = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Please enter your gender (M or F)");
+char gender = Convert.ToChar(Console.ReadLine());
+
+
+
+// Interpolation
+Console.WriteLine($"Your age is:{age}");
+
+
