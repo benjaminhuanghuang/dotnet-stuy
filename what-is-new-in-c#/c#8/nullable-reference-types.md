c1bd70cacf29e18172fb93f8d26add3ab3b956db
BenjaminHuang <benjaminhuanghuang@gmail.com>
2023-02-26 18:51:11 -0800

    update

---
+++ what-is-new-in-c#/c#8/nullable-reference-types.md
@@ -0,0 +1,9 @@
+# Nullable reference types (C# reference)
+https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/nullable-reference-types
+
+
+## Enable the nullable context in the project file
+```
+  <Nullable>enable</Nullable>
+```
+Note: All project templates starting with .NET 6 (C# 10) enable the nullable context for the project. 
--- what-is-new-in-c#/c#8demo/c#8demo/Program.cs
+++ what-is-new-in-c#/c#8demo/c#8demo/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+var questions = new List<SurveyQuestion>
+{
+    new SurveyQuestion("Q1", 1)
+};
+
+var q2 = new SurveyQuestion("Q2", 2);
+
+questions.Add(q2);
+
+foreach (var q in questions)
+{
+    // Nullable
+    var hint = q.QuestionHint == null ? string.Empty : $"(hint: {q.QuestionHint})";
+
+    Console.WriteLine($"{q.QuestionText}, {hint}");
+}
+
+public class SurveyQuestion
+{
+    public SurveyQuestion(string text, int number) =>
+        (QuestionNumber, QuestionText) = (number, text);
+
+    public string QuestionText { get; }
+
+    public int QuestionNumber { get; }
+
+    public string? QuestionHint { get; }
+
+}
+
+
