e35bc59ea1497f149e0ff9d980c3752d64a8154f
BenjaminHuang <benjaminhuanghuang@gmail.com>
2023-05-24 23:00:04 -0700

    update

--- ASP.NET Core/Razor_Pages/Razor Pages for ASP.NET Core - Full Course/Abby/AbbyWeb/Pages/Categories/Create.cshtml.cs
+++ ASP.NET Core/Razor_Pages/Razor Pages for ASP.NET Core - Full Course/Abby/AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -1,3 +1,4 @@
+using AbbyWeb.Data;
 using AbbyWeb.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -6,10 +7,26 @@ namespace AbbyWeb.Pages.Categories
 {
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
         public Category Category { get; set; }
             
+        public CreateModel(ApplicationDbContext db)
+        {
+            this._db = db;
+        }
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _db.Category.Add(category);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
         }
     }
 }
