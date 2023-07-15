78624853f82fbddd31e786c7dfd6078150c37a04
BenjaminHuang <benjaminhuanghuang@gmail.com>
2023-07-14 23:33:38 -0700

    update

---
+++ ASP.NET Core/Web API/SuperHeroAPI/README.md
@@ -0,0 +1,8 @@
+# .NET 7 Web API & Entity Framework 🚀 Full Course (CRUD, Repository Pattern, DI, SQL Server & more)
+https://youtube.com/watch?v=8pH5Lv4d5-g
+
+
+Create Models
+
+
+Create an API Controller - Empty
---
+++ ASP.NET Core/Web API/SuperHeroAPI/SuperHeroAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperHeroAPI.Models;
+
+namespace SuperHeroAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        [HttpGet]
+        
+        public async Task<ActionResult<List<SuperHero>>> GetAllHeros()
+        {
+            var superHeros = new List<SuperHero> { 
+                new SuperHero { Id = 1, Name="a", FirstName= "a", LastName="a", Place= "New York City"}
+            };
+
+
+            return Ok(superHeros);
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<SuperHero>> GetSingleHeros(int id)
+        {
+            var superHeros = new List<SuperHero> {
+                new SuperHero { Id = 1, Name="a", FirstName= "a", LastName="a", Place= "New York City"}
+            };
+
+
+            return Ok(superHeros);
+        }
+    }
+}
