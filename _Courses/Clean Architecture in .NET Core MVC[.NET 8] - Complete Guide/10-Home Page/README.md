dc869072f5274cc5eb372d3401de862acede53c9
BenjaminHuang <benjaminhuanghuang@gmail.com>
2023-12-31 21:59:35 -0800

    Home

---
+++ _Courses/Clean Architecture in .NET Core MVC[.NET 8] - Complete Guide/10-Home Page/README.md
@@ -0,0 +1,29 @@
+
+## Dropdown in Navbar
+Search Navbar in bootstrap copy the dropdown code and paste in _Layout.cshtml
+Add bootstrap.bundle.min.js CDN link in _Layout.cshtml
+
+Remove css from _Layout.cshtml.css
+```
+a.navbar-brand {
+  white-space: normal;
+  text-align: center;
+  word-break: break-all;
+}
+
+a {
+  color: #0077cc;
+}
+```
+
+## Home Page
+Add HomeVM
+
+
+## Villa List
+
+
+## Villa Detail
+
+
+## Partial View
--- _Courses/Clean Architecture in .NET Core MVC[.NET 8] - Complete Guide/WhiteLagoon/WhiteLagoon.Domain/Entities/Villa.cs
+++ _Courses/Clean Architecture in .NET Core MVC[.NET 8] - Complete Guide/WhiteLagoon/WhiteLagoon.Domain/Entities/Villa.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -33,5 +34,8 @@ namespace WhiteLagoon.Domain.Entities
         
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdateDate { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<Amenity> VillaAmenity { get; set; }
     }
 }
