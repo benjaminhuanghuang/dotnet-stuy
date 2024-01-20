dc8fb6ea0abd21978e93c6fb90d0a4371d6824f7
BenjaminHuang <benjaminhuanghuang@gmail.com>
2024-01-19 21:55:46 -0800

    update

--- Projects/dotNet8WebAPI-angular17-CRUD/Api/Controller/StudentController.cs
+++ Projects/dotNet8WebAPI-angular17-CRUD/Api/Controller/StudentController.cs
@@ -48,11 +48,61 @@ namespace Api.Controller
 
             await _context.AddAsync(student);
             var result = await _context.SaveChangesAsync();
-            if(result > 0)
+            if (result > 0)
             {
                 return Ok(student);
             }
             return BadRequest();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Student>> GetStudent(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student is null)
+            {
+                return NotFound();
+            }
+            return Ok(student);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student is null)
+            {
+                return NotFound();
+            }
+            _context.Remove(student);
+            var result = await _context.SaveChangesAsync();
+            if (result > 0)
+            {
+                return Ok("student deleted successfully");
+            }
+            return BadRequest("Unable to delete student");
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> EditStudent(int id, Student student)
+        {
+            var studentFromDb = await _context.Students.FindAsync(id);
+            if (studentFromDb is null)
+            {
+                return NotFound();
+            }
+
+            studentFromDb.Name = student.Name;
+            studentFromDb.Address = student.Address;
+            studentFromDb.Email = student.Email;
+            studentFromDb.PhoneNumber = student.PhoneNumber;
+
+            var result = await _context.SaveChangesAsync();
+            if (result > 0)
+            {
+                return Ok("student updated successfully");
+            }
+            return BadRequest("Unable to update student");
         }
     }
 }
